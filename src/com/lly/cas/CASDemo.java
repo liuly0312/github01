4cc342327b7c2a23f249a6e778d7e2f4a8076b74
liuluyang <liuluyang0312@163.com>
2020-11-24 15:30:20 +0800

    master

---
+++ src/com/lly/cas/CASDemo.java
@@ -0,0 +1,11 @@
+package com.lly.cas;
+
+public class CASDemo {
+
+    public static void main(String[] args) {
+        while (true) {
+            int inu = 0;
+            System.out.println(inu++);
+        }
+    }
+}
